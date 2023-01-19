004f22f0e1f999e2e21c80417acc3bdc615da3a2
GetRawFish <mail@getrawfish.com>
2023-01-19 15:57:41 +0000

    Contact animation

---
+++ src/assets/js/_contact.js
@@ -0,0 +1,84 @@
+const contactTitleContainer = document.querySelector("#contact-title-container");
+const contactTitle = document.querySelector("#contact-title");
+
+let intervalId;
+
+function startAnimation() {
+  addClonedElements();
+
+  const clones = numberToClone();
+  // create array with positions of elements
+  let translatePosition = new Array(clones + 1).fill(0);
+
+  intervalId = setInterval(createAnimation, 10);
+
+  function createAnimation() {
+    contactTitleContainer.childNodes.forEach((el, i) => {
+      // update element position
+      el.style.transform = `translateX(${translatePosition[i]}px)`;
+      translatePosition[i] = translatePosition[i] - 1;
+
+      if (Math.round(el.getBoundingClientRect().right) === 0) {
+        // safe new position for moved element
+        translatePosition[i] = Math.round(contactTitleContainer.childNodes[clones].getBoundingClientRect().right);
+
+        // move first element to the end
+        const element = el;
+        contactTitleContainer.removeChild(element);
+        contactTitleContainer.appendChild(element);
+      }
+    });
+  }
+};
+
+function stopInterval() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+function addClonedElements() {
+  // create range of elelemnts to be cloned
+  const cloneRange = range(1, numberToClone());
+
+  // update container content
+  contactTitleContainer.replaceChildren(contactTitle);
+
+  // clone elelemnts
+  cloneRange.forEach((i) => {
+    const clone = contactTitle.cloneNode(true);
+    // add unique id for each cloned element
+    clone.id = `contact-title-clone-${i}`;
+    contactTitleContainer.appendChild(clone);
+  });
+};
+
+function numberToClone() {
+  // calculate number of cloned elelemnts
+  return Math.ceil(contactTitleContainer.offsetWidth / contactTitle.offsetWidth);
+};
+
+function range(start, end) {
+  // create range
+  return (new Array(end - start + 1))
+    .fill(undefined)
+    .map((_, i) => i + start
+  );
+};
+
+const resizeObserver = new ResizeObserver(() => {
+  stopInterval();
+  startAnimation();
+});
+resizeObserver.observe(contactTitleContainer);
+
+const intersectionObserverOptions = { threshold: 0 }
+const intersectionObserverCallback = (entries) => {
+  for (const entry of entries) {
+    entry.isIntersecting
+      ? startAnimation()
+      : stopInterval();
+  }
+};
+
+const intersectionObserver = new IntersectionObserver(intersectionObserverCallback, intersectionObserverOptions);
+intersectionObserver.observe(contactTitleContainer);
