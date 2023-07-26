2ba350a3bdeb6dfed2a0d679e3c8fa1b98667456
Andros Fenollosa <andros@fenollosa.email>
2023-07-26 17:49:13 +0200

    Fix address

--- src/_active-event.php
+++ src/_active-event.php
@@ -14,7 +14,7 @@ $pathCover = 'src/assets/img/talks/cover.webp';
       <p class="events__all--info__description"><?= $event['description'] ?></p>
       <time datetime="<?= str_replace(" ", "T", $event['datetime']) ?>" class="events__all--info__date label white"><?= $event['date_format'] ?></time>
       <time aria-hidden="true" datetime="<?= $event['hour_format'] ?>" class="events__all--info__time label white"><?= $event['hour_format'] ?>h</time>
-      <p class="events__all--info__location label white">WayCo Ruzafa - Carrer de l'Almirall Cadarso, 26, 46005 València, Valencia</p>
+      <p class="events__all--info__location label white">WayCO Cabanyal  - Carrer de Marià Cuber, 17, 46011 València, Valencia</p>
     </section>
     <figure class="events__all--photo grid-layout--with-photo__photo">
       <img src="<?= $event['url_image'] ?>" alt="Conferenciante" class="events__img">
